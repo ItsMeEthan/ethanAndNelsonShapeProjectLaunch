c3b7cb1b60ac879a64a8712f4b2475866c5ef376
BT_1N3_05 <epersaud0841@bths.edu>
2018-10-16 08:39:01 -0400

    yes

--- src/ShapeProject/Shape.java
+++ src/ShapeProject/Shape.java
@@ -1,14 +1,18 @@
 package ShapeProject;
 
-public abstract class Shape {
-    private String type;
-    public Shape(String type){
-        this.type= type;
-    }
-    public abstract double getArea();
-    public abstract double getPerimeter;
-    public String toString()
-    {
-        return "I am a " + type + ".";
+public class Shape {
+    package ShapeProject;
+
+    public abstract class Shape {
+        private String type;
+        public Shape(String type){
+            this.type= type;
+        }
+        public abstract double getArea();
+        public abstract double getPerimeter;
+        public String toString()
+        {
+            return "I am a " + type + ".";
+        }
     }
 }
